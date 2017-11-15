a92732b8e75f928e7a843476e8d0d8e678f5f659
soulia <soulia@aol.com>
2017-11-14 20:36:05 -0500

    Go Live with Spanish

--- CHSAuction.Web/Controllers/BidsController.cs
+++ CHSAuction.Web/Controllers/BidsController.cs
@@ -119,7 +119,7 @@ namespace CHSAuction.Web.Controllers
         }
 
         [HttpGet]
-        [Authorize(Users ="mike.soulia@gmail.com")]
+        [Authorize]
         public ActionResult Create(int itemId)
         {
             var allBids = from b in _db.ItemBids
@@ -147,7 +147,7 @@ namespace CHSAuction.Web.Controllers
 
         // POST: Bids/Create
         [HttpPost]
-        [Authorize(Users = "mike.soulia@gmail.com")]
+        [Authorize]
         // Obscure naming convention bug with EF - don't name parameters the same as model properties...
         // http://www.martin-brennan.com/net-mvc-4-model-binding-null-on-post/
         public ActionResult Create(ItemBid bidd)
